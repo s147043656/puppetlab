fb8719b5d9df66dde7c06fb123b0bc0c105923b1
VT <s147043656@gmail.com>
2016-08-08 15:58:48 +0300

    Update init script

--- bash-scripts/init.sh
+++ bash-scripts/init.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-# Install Git
+echo "### Installing git:"
 yum install -y git-core
 
-# Clone puppetlab HEAD
+echo "### Cloning puppetlab repo from HEAD:"
 git clone git@github.com:s147043656/puppetlab.git
 
-# Install scripts for git updates and puppet applying
+echo "### Installing scripts for automated git update and puppet applying:"
 cd puppetlab/bash-scripts
 cp gitupdate.sh /usr/local/bin && chmod 755 /usr/local/bin/gitupdate.sh && chown root.root /usr/local/bin/gitupdate.sh
 cp papply.sh /usr/local/bin && chmod 755 /usr/local/bin/papply.sh && chown root.root /usr/local/bin/papply.sh
 
-# Create master users
+echo "###Creating of  master user:"
 useradd -m master
 
-# Install puppet-server
+echo "### Installing puppet-server:"
 yum install -y epel-release
 yum update
 yum install -y puppet-server
 
-#Add cron jobs for root ans master users
+echo "### Adding cron jobs for root and master:"
 { crontab -l -u master; echo '*/1 * * * * /usr/local/bin/gitupdate.sh'; } | crontab -u master -
 { crontab -l -u root; echo '*/1 * * * * /usr/local/bin/papply.sh'; } | crontab -u root -
+
+echo "### Done."
