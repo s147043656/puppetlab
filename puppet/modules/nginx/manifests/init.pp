153d65f0c1931eb409acf094adea9122c31062bb
VT <s147043656@gmail.com>
2016-08-10 12:12:17 +0300

    Add firewall rule for port 80

--- puppet/modules/nginx/manifests/init.pp
+++ puppet/modules/nginx/manifests/init.pp
@@ -1,12 +1,26 @@
 # Manage Nginx web server
 
 class nginx {
+
   package { 'nginx':
     ensure => installed,
   }
+
   service { 'nginx':
     ensure => running,
     enable => true,
     require => Package['nginx'],
   }
+
+  exec { 'Open port 80':
+    command  => "/sbin/iptables -I INPUT 5 -m state --state NEW -p tcp --dport 80 -j ACCEPT",
+    unless   => "/sbin/iptables-save | grep 80 | grep INPUT | grep ACCEPT | grep NEW | wc -l | xargs test 1 -le",
+#    notify   => Exec["Save iptables"]
+  }
+
+#  exec { 'Save iptables':
+#    command     => "service iptables save",
+#    refreshonly => true,
+  }
+
 }
