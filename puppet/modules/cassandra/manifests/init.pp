3bf9ea6c323d1c37449d1aeb09554b40e43f6df1
VT <s147043656@gmail.com>
2016-08-15 15:35:07 +0300

    set path

--- puppet/modules/cassandra/manifests/init.pp
+++ puppet/modules/cassandra/manifests/init.pp
@@ -3,12 +3,14 @@
 class cassandra {
 
   exec { 'Build cassandra docker image':
-    command => "/usr/bin/cd /home/master/git-develop/puppet/modules/cassandra/files && /bin/bash build_images.sh",
+    command => 'cd /home/master/git-develop/puppet/modules/cassandra/files && /bin/bash build_images.sh',
+    path => '/usr/bin',
     require => Package['docker-engine'],
   }
 
   exec { 'Start cassandra cluster':
-    command => "/usr/bin/cd /home/master/git-puppetlab/puppet/modules/cassandra/files/scripts && /bin/bash start_docker_cluster.sh cassandra",
+    command => 'cd /home/master/git-puppetlab/puppet/modules/cassandra/files/scripts && /bin/bash start_docker_cluster.sh cassandra',
+    path => '/usr/bin',
     require => Exec['Build cassandra docker image'],
   }
 
