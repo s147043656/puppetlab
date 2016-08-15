99c47c51928d64957600980ee6865150a95ecaf0
VT <s147043656@gmail.com>
2016-08-15 15:20:30 +0300

    set path for cd

--- puppet/modules/cassandra/manifests/init.pp
+++ puppet/modules/cassandra/manifests/init.pp
@@ -3,12 +3,12 @@
 class cassandra {
 
   exec { 'Build cassandra docker image':
-    command => "cd /home/master/git-puppetlab/puppet/modules/cassandra/files && /bin/bash build_images.sh",
+    command => "/usr/bin/cd /home/master/git-puppetlab/puppet/modules/cassandra/files && /bin/bash build_images.sh",
     require => Package['docker-engine'],
   }
 
   exec { 'Start cassandra cluster':
-    command => "cd /home/master/git-puppetlab/puppet/modules/cassandra/files/scripts && /bin/bash start_docker_cluster.sh cassandra",
+    command => "/usr/bin/cd /home/master/git-puppetlab/puppet/modules/cassandra/files/scripts && /bin/bash start_docker_cluster.sh cassandra",
     require => Exec['Build cassandra docker image'],
   }
 
