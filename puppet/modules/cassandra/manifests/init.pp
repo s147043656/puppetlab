8a24872e271600c4a4fdb1c90462b46fdf064123
VT <s147043656@gmail.com>
2016-08-15 17:21:54 +0300

    Set timeout 1800 for cassandra tasks

--- puppet/modules/cassandra/manifests/init.pp
+++ puppet/modules/cassandra/manifests/init.pp
@@ -6,12 +6,14 @@ class cassandra {
     command => 'cd /home/master/git-develop/puppet/modules/cassandra/files && /bin/bash build_images.sh',
     unless => 'docker images | grep cassandra',
     path => '/usr/bin',
+    timeout => 1800,
     require => Package['docker-engine'],
   }
 
   exec { 'Start cassandra cluster':
     command => 'cd /home/master/git-develop/puppet/modules/cassandra/files/scripts && /bin/bash start_docker_cluster.sh cassandra',
     path => '/usr/bin',
+    timeout => 1800,
     require => Exec['Build cassandra docker image'],
   }
 
