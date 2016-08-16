5471f94859df580eca98aa4986417c392c8ecbfc
VT <s147043656@gmail.com>
2016-08-16 17:25:22 +0300

    comment duplicate

--- puppet/modules/network-config/manifests/init.pp
+++ puppet/modules/network-config/manifests/init.pp
@@ -12,10 +12,10 @@ class network-config {
     }
   }
 
-  set_ip_addr { 'Configure host only adapter':
-    iface_name => 'enp0s8',
-    ip_addr => '192.168.7.16',
-    notify => Service['network'],
-  }
+#  set_ip_addr { 'Configure host only adapter':
+#    iface_name => 'enp0s8',
+#    ip_addr => '192.168.7.16',
+#    notify => Service['network'],
+#  }
 
 }
