e6e8ed767a6c2b20d906844de8007ad89b82d454
VT <s147043656@gmail.com>
2016-08-16 17:24:02 +0300

    use qualified name

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -56,5 +56,11 @@ node 'c7i16.vtdomain' {
   include base
   include docker
   include swarm
-  $vbox_ho_ip_addr = "192.168.7.16"
+
+  network-config::set_ip_addr { 'Configure host only adapter':
+    iface_name => 'enp0s8',
+    ip_addr => '192.168.7.16',
+    notify => Service['network'],
+  }
+
 }
--- puppet/modules/network-config/manifests/init.pp
+++ puppet/modules/network-config/manifests/init.pp
@@ -14,7 +14,7 @@ class network-config {
 
   set_ip_addr { 'Configure host only adapter':
     iface_name => 'enp0s8',
-    ip_addr => $vbox_ho_ip_addr,
+    ip_addr => '192.168.7.16',
     notify => Service['network'],
   }
 
