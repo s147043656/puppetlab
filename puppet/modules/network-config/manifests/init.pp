17942272cffab5ff02a28b7caeebb736f5c93a7c
VT <s147043656@gmail.com>
2016-08-16 16:23:31 +0300

    fix resource declaration

--- puppet/modules/network-config/manifests/init.pp
+++ puppet/modules/network-config/manifests/init.pp
@@ -3,7 +3,7 @@
 class network-config {
 
   define set_ip_address ( $iface_name = $title, $ip_address ) {
-    file { "/etc/sysconfig/network-scripts/ifcfg-${iface_name}",
+    file { "/etc/sysconfig/network-scripts/ifcfg-${iface_name}":
       content => template('network-config/files/eth.erb'),
     }
   }
