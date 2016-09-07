0275b33692955788b44836b42e1dfb51302d261b
VT <s147043656@gmail.com>
2016-09-07 17:39:06 +0300

    Add iptables rule for docker machines to open tcp port 2377

--- puppet/modules/docker/manifests/init.pp
+++ puppet/modules/docker/manifests/init.pp
@@ -17,4 +17,9 @@ class docker {
     require => Package['docker-engine'],
   }
 
+  exec { 'Open port 2377':
+    command  => "/sbin/iptables -I INPUT 5 -m state --state NEW -p tcp --dport 2377 -j ACCEPT",
+    unless   => "/sbin/iptables-save | grep 2377 | grep INPUT | grep ACCEPT | grep NEW | wc -l | xargs test 1 -le",
+  }
+
 }
