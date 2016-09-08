a9882f7913bc52966ba578277395cd8cf3e411e9
VT <s147043656@gmail.com>
2016-09-08 14:00:00 +0300

    Add bind-utils to base module

--- puppet/modules/base/manifests/init.pp
+++ puppet/modules/base/manifests/init.pp
@@ -1,7 +1,12 @@
 # Manage standard set of packages
 
 class base {
-  package { [ 'mailx', 'vim-enhanced', 'mc', 'wget', 'telnet' ]:
+  package { [ 'mailx',
+              'vim-enhanced',
+              'mc', 'wget',
+              'telnet',
+              'bind-utils' 
+            ]:
     ensure => installed,
   }
   package { 'tcpdump':
