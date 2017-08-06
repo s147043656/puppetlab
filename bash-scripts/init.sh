efd2892f0daa60086f9558688a91418f8828fd1d
VT <s147043656@gmail.com>
2017-08-06 09:24:40 +0300

    add Ubuntu distribution

--- bash-scripts/init.sh
+++ bash-scripts/init.sh
@@ -8,6 +8,30 @@ if [ $# -lt 1 ];
   exit 1
 fi
 
+if [ -f /etc/redhat-release ]; then
+  if [ `head -1 /etc/issue | awk '{print $1}'` == "RedHat" ]; then
+    distrType=RedHat
+  elif
+    [ `head -1 /etc/issue | awk '{print $1}'` == "CentOS" ]; then
+      distrType=CentOS
+  fi
+elif
+  [ `head -1 /etc/issue | awk '{print $1}'` == "Ubuntu" ]; then
+  distrType=Ubuntu
+fi
+
+case $distrType in
+  RedHat|Centos|Debian|Ubuntu)
+    echo "Starting bootstrap for $distrType"
+    ;;
+  *)
+    echo "Distributive is not defined or not supported." && exit 1
+    ;;
+esac
+
+echo "Starting bootstrap for $distrType"
+
+
 echo "### Renaming host to new name $1:"
 hostnamectl set-hostname $1
 echo "### Done."
@@ -26,7 +50,14 @@ chown -R ${userName}.${userName} /home/${userName}/.ssh
 echo "### Done."
 
 echo "### Installing pupper-server:"
-yum install -y puppet-server
+  case $distrType in
+    RedHat|Centos)
+      yum install -y puppet-server
+      ;;
+    Debian|Ubuntu)
+      apt-get -y install puppetmaster
+      ;;
+  esac
 echo "### Done."
 
 echo "### Cron for ${userName}:"
