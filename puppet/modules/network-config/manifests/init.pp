fb0cb1d0aee8e9c6b54225662020b63d20b072ac
VT <s147043656@gmail.com>
2017-08-06 15:27:44 +0300

    network configuration for debian

--- puppet/modules/network-config/manifests/init.pp
+++ puppet/modules/network-config/manifests/init.pp
@@ -6,9 +6,15 @@ class network-config {
     ensure => 'running',
   }
 
-  define set_ip_addr ( $iface_name, $ip_addr ) {
+  define set_ip_addr_rh ( $iface_name, $ip_addr ) {
     file { "/etc/sysconfig/network-scripts/ifcfg-${iface_name}":
-      content => template('network-config/eth.erb'),
+      content => template('network-config/eth-rh.erb'),
+    }
+  }
+
+  define set_ip_addr_deb ( $iface_name, $ip_addr ) {
+    file { "/etc/network/interfaces":
+      content => template('network-config/eth-deb.erb'),
     }
   }
 
