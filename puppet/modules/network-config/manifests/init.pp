c5c59a250448b24752779e97941512fda83a6867
VT <s147043656@gmail.com>
2017-08-07 11:29:10 +0300

    divide network and networking service for rh and deb

--- puppet/modules/network-config/manifests/init.pp
+++ puppet/modules/network-config/manifests/init.pp
@@ -2,17 +2,23 @@
 
 class network-config {
 
-  service {'network':
-    ensure => 'running',
-  }
+#  service {'network':
+#    ensure => 'running',
+#  }
 
   define set_ip_addr_rh ( $iface_name, $ip_addr ) {
+    service {'network':
+      ensure => 'running',
+    }
     file { "/etc/sysconfig/network-scripts/ifcfg-${iface_name}":
       content => template('network-config/eth-rh.erb'),
     }
   }
 
   define set_ip_addr_deb ( $iface_name, $ip_addr ) {
+    service {'networking':
+      ensure => 'running',
+    }
     file { "/etc/network/interfaces":
       content => template('network-config/eth-deb.erb'),
     }
