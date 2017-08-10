100a26bca1a27ef51150235f7fbe61a9e4b543fa
VT <s147043656@gmail.com>
2017-08-10 13:42:16 +0300

    set new log dest

--- bash-scripts/papply5.sh
+++ bash-scripts/papply5.sh
@@ -2,4 +2,4 @@
 
 . /usr/local/etc/deployment-variables
 
-puppet apply ${gitDir}/puppet5/manifests/site.pp --modulepath=${gitDir}/puppet5/modules/ --logdest /var/log/papply5.log $*
+puppet apply ${gitDir}/puppet5/manifests/site.pp --modulepath=${gitDir}/puppet5/modules/ --logdest /var/log/puppetlabs/puppet/papply5.log $*
