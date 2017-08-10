88e527915d3afdb5edf6bf7a51e0635c147177de
VT <s147043656@gmail.com>
2017-08-10 22:23:57 +0300

    specify puppet path

--- bash-scripts/papply5.sh
+++ bash-scripts/papply5.sh
@@ -2,5 +2,8 @@
 
 . /usr/local/etc/deployment-variables
 
-puppet apply ${gitDir}/puppet5/manifests/site.pp --modulepath=${gitDir}/puppet5/modules/ --logdest /var/log/puppetlabs/puppet/papply5.log $*
+/opt/puppetlabs/bin/puppet apply ${gitDir}/puppet5/manifests/site.pp \
+  --modulepath=${gitDir}/puppet5/modules/ \
+  --logdest /var/log/puppetlabs/puppet/papply5.log \
+  $*
 
