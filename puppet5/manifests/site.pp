9c91cfe1d900a1d9270500b6eab146a723476385
VT <s147043656@gmail.com>
2017-08-11 12:33:50 +0300

    c7 and u16 nodes

--- puppet5/manifests/site.pp
+++ puppet5/manifests/site.pp
@@ -6,3 +6,19 @@ node default {
   }
   include base_all
 }
+
+node c7i2 {
+  file { '/tmp/hello':
+    content => "Hello c7i2!\n",
+  }
+  include base_all
+  include base_centos7
+}
+
+node u16i102 {
+  file { '/tmp/hello':
+    content => "Hello u16i102!\n",
+  }
+  include base_all
+  include base_ubuntu1604
+}
