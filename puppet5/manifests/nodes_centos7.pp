4b0c38ec464d48c4c51da8d82cd12b934942dc8d
VT <s147043656@gmail.com>
2017-08-19 23:01:46 +0300

    add i1 nodes

--- puppet5/manifests/nodes_centos7.pp
+++ puppet5/manifests/nodes_centos7.pp
@@ -1,5 +1,18 @@
 ### Centos 7 nodes
 
+node c7i1 {
+  file { '/tmp/hello':
+    content => "Hello c7i1!\n",
+  }
+  include network_config
+    network_config::set_ip_addr_rh { 'Configure host only adapter':
+      iface_name => 'enp0s8',
+      ip_addr => '192.168.7.1',
+  }
+  include base_all
+  include base_centos7
+}
+
 node c7i2 {
   file { '/tmp/hello':
     content => "Hello c7i2!\n",
--- puppet5/manifests/nodes_ubuntu1604.pp
+++ puppet5/manifests/nodes_ubuntu1604.pp
@@ -1,5 +1,18 @@
 ### Ubuntu 16.04 nodes
 
+node u16i1 {
+  file { '/tmp/hello':
+    content => "Hello u16i1!\n",
+  }
+  include network_config
+    network_config::set_ip_addr_deb { 'Configure host only adapter':
+      iface_name => 'enp0s8',
+      ip_addr => '192.168.7.101',
+  }
+  include base_all
+  include base_ubuntu1604
+}
+
 node u16i2 {
   file { '/tmp/hello':
     content => "Hello u16i2!\n",
