07d458fcc35cdb7ccec736db5c67d9c4bd320b28
VT <s147043656@gmail.com>
2017-08-19 12:05:55 +0300

    revert

--- puppet5/manifests/nodes_ubuntu1604.pp
+++ puppet5/manifests/nodes_ubuntu1604.pp
@@ -3,6 +3,6 @@ node u16i102 {
   file { '/tmp/hello':
     content => "Hello u16i102!\n",
   }
-#  include base_all
+  include base_all
   include base_ubuntu1604
 }
--- puppet5/manifests/site.pp
+++ puppet5/manifests/site.pp
@@ -4,5 +4,4 @@ node default {
   file { '/tmp/hello':
     content => "Hello Default!\n",
   }
-  include base_all
 }
