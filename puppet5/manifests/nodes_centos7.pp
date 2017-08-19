44a5b4c02bb013b50e6f43680e262d525a23c9ff
VT <s147043656@gmail.com>
2017-08-19 12:56:40 +0300

    remove notify

--- puppet5/manifests/nodes_centos7.pp
+++ puppet5/manifests/nodes_centos7.pp
@@ -7,7 +7,6 @@ node c7i2 {
     network_config::set_ip_addr_rh { 'Configure host only adapter':
       iface_name => 'enp0s8',
       ip_addr => '192.168.7.2',
-      notify => Service['network'],
   }
   include base_all
   include base_centos7
