acc2dbce48c16e47bfaffbc04c53dee6c18af9ae
VT <s147043656@gmail.com>
2017-08-19 12:36:31 +0300

    underscore fix

--- puppet5/modules/network_config/manifests/init.pp
+++ puppet5/modules/network_config/manifests/init.pp
@@ -1,16 +1,16 @@
 # Manage network configuration
 
-class network-config {
+class network_config {
 
   define set_ip_addr_rh ( $iface_name, $ip_addr ) {
     file { "/etc/sysconfig/network-scripts/ifcfg-${iface_name}":
-      content => template('network-config/eth-rh.erb'),
+      content => template('network_config/eth-rh.erb'),
     }
   }
 
   define set_ip_addr_deb ( $iface_name, $ip_addr ) {
     file { "/etc/network/interfaces":
-      content => template('network-config/eth-deb.erb'),
+      content => template('network_config/eth-deb.erb'),
     }
   }
 
