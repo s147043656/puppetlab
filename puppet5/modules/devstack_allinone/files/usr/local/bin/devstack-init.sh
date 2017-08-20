2dff6d706b8738a7a2d8e7993b3ac3db75cc690a
VT <s147043656@gmail.com>
2017-08-20 14:45:03 +0300

    fix devstack local.conf copy

--- puppet5/modules/devstack_allinone/files/usr/local/bin/devstack-init.sh
+++ puppet5/modules/devstack_allinone/files/usr/local/bin/devstack-init.sh
@@ -3,5 +3,5 @@
 useradd -s /bin/bash -d /opt/stack -m stack
 echo "stack ALL=(ALL) NOPASSWD: ALL" | tee /etc/sudoers.d/stack
 su - stack -c "git clone https://git.openstack.org/openstack-dev/devstack"
-su - stack -c "cd devstack && cp /usr/local/etc/devstack_local.conf ."
+su - stack -c "cd devstack && cp /usr/local/etc/devstack_local.conf local.conf"
 su - stack -c "cd devstack && ./stack.sh"
