efff6bc216c138a3b9a2a150e3aaedde241a3006
VT <s147043656@gmail.com>
2017-08-20 17:17:41 +0300

    kubernetes module refactoring

--- puppet5/modules/kubernetes_allinone/manifests/init.pp
+++ puppet5/modules/kubernetes_allinone/manifests/init.pp
@@ -1,38 +1,71 @@
-# Manage Kubernetes server
+# Manage Kubernetes All in One server
+# https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux_atomic_host/7/html/getting_started_with_kubernetes/get_started_orchestrating_containers_with_kubernetes
 
 class kubernetes_allinone {
 
-  package { 'docker':
-    ensure => installed,
-  }
-
-  service { 'docker':
-    ensure => running,
-    enable => true,
-    require => Package['docker'],
-  }
+# 1.3.1 - 2. Install Kubernetes
 
   package { 'kubernetes-client':
     ensure => installed,
   }
-
   package { 'kubernetes-node':
     ensure => installed,
   }
-
   package { 'etcd':
     ensure => installed,
   }
 
-  service { 'etcd':
-    ensure => running,
-    enable => true,
-    require => Package['etcd'],
-  }
+# 1.3.1 - 3. Disable firewall
 
   service { 'firewalld':
     ensure => stopped,
     enable => false,
   }
+
+# 1.3.1 - 5. Install registry
+
+  package { 'docker-distribution':
+    ensure => installed,
+  }
+
+# 1.3.1 - 6. Start the local Docker Registry
+
+  service { 'docker-distribution':
+    ensure => running,
+    enable => true,
+    require => Package['docker-distribution'],
+  }
+
+# 1.3.2 - 2. Create manifest files
+# 1.3.2 - 3. Cofigure the kublet service
+
+  file { '/etc/kubernetes':
+    ensure => directory,
+    source => "puppet:///modules/kubernetes_allinone/etc",
+    recurse => 'true',
+  }
+
+# Install bootstrap script
+
+  file { '/usr/local/bin/kubernetes-init.sh':
+    source => "puppet:///modules/kubernetes_allinone/usr/local/bin/kubernetes-init.sh",
+  }
+
+#  package { 'java-1.8.0-openjdk':
+#    ensure => installed,
+#  }
+#  package { 'docker':
+#    ensure => installed,
+#  }
+#  service { 'docker':
+#    ensure => running,
+#    enable => true,
+#    require => Package['docker'],
+#  }
+#  service { 'etcd':
+#    ensure => running,
+#    enable => true,
+#    require => Package['etcd'],
+#  }
 
 }
