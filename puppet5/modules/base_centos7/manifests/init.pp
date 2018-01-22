c5c2789720789dc647f48598208bdc79bd44bbb5
VT <s147043656@gmail.com>
2018-01-22 15:04:07 +0200

    add telnet and nslookup to centos7 base

--- puppet5/modules/base_centos7/manifests/init.pp
+++ puppet5/modules/base_centos7/manifests/init.pp
@@ -1,7 +1,9 @@
 # Manage standard set of packages
 
 class base_centos7 {
-  package { [ 'mailx'
+  package { [ 'mailx',
+              'bind-utils',
+              'telnet',
             ]:
     ensure => installed,
   }
